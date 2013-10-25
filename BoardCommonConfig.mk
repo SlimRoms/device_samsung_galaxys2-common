76dfc51429c123a1e2d9f733e503eb5588821478
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2013-10-25 23:44:05 +0100

    Get rid of the gfx buffer workarounds
    Concurrent buffers are now working, we don't need this crap anymore
    
    Change-Id: I80c35bb67c41e9c61faeb6f3cd80d3e9033f9641

--- BoardCommonConfig.mk
+++ BoardCommonConfig.mk
@@ -70,7 +70,6 @@ TARGET_RELEASETOOL_IMG_FROM_TARGET_SCRIPT := ./device/samsung/galaxys2-common/re
 # Graphics
 BOARD_EGL_CFG := device/samsung/galaxys2-common/configs/egl.cfg
 USE_OPENGL_RENDERER := true
-BOARD_EGL_NEEDS_LEGACY_FB := true
 
 # FIMG Acceleration
 BOARD_USES_FIMGAPI := true
